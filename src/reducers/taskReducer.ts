c42a549013ff99d3b721f213955c518fc68fb991
lexggwp <alexeyfilipp@gmail.com>
2022-08-05 19:27:39 +0300

    feat: example for reducer

---
+++ src/reducers/taskReducer.ts
@@ -0,0 +1,10 @@
+export const taskReducer = (state: any, action: any) => {
+    switch (action.type) {
+        case 'XXX':
+            return state;
+        case 'YYY':
+            return state;
+        default:
+            return state;
+    }
+}

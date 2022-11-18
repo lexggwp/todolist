f7f00cca76941c99ed0b059002e814f3e5c021dd
lexggwp <alexeyfilipp@gmail.com>
2022-11-18 13:45:34 +0300

    added: new logic for EditableSpan.tsx - toggleMode

--- src/Components/EditableSpan.tsx
+++ src/Components/EditableSpan.tsx
@@ -9,20 +9,22 @@ type EditableSpanPropsType = {
 const EditableSpan = (props: EditableSpanPropsType) => {
     const [editView, setEditView] = useState(false);
     const [inputValue, setInputValue] = useState(props.inputValue)
-    const onClickEditViewHandler = () => {
-        setEditView(!editView)
-    }
-    const onBlurEditHandler = () => {
-        setEditView(!editView)
-        props.changeTaskInputValue(inputValue);
-    }
+
     const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
         setInputValue(event.currentTarget.value)
     }
 
+    const toggleMode = () => {
+        setEditView(!editView);
+        if (editView && inputValue !== props.inputValue && inputValue.length) {
+            props.changeTaskInputValue(inputValue);
+        } else setInputValue(props.inputValue);
+    }
+
     return (
-        editView ?  <TextField variant={'standard'} onChange={onChangeHandler} onBlur={onBlurEditHandler} value={inputValue} autoFocus/> :
-            <span onDoubleClick={onClickEditViewHandler} >{props.inputValue}</span>
+        editView ? <TextField variant={'standard'} onChange={onChangeHandler} onBlur={toggleMode} value={inputValue}
+                              autoFocus/> :
+            <span onDoubleClick={toggleMode}>{props.inputValue}</span>
     );
 };
 
